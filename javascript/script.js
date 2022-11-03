5b1939d3b1c8cb7fdf59b1fda397b40942880499
Garox7 <giuseppe.garozzo2@icloud.com>
2022-11-03 17:32:10 +0100

    Commit for check

--- javascript/script.js
+++ javascript/script.js
@@ -9,14 +9,24 @@ playButton.addEventListener('click', function(){
     popupElement.classList.add('hidden');
     gridElement.classList.remove('hidden');
 
-    //inserisco il il valore del livello di difficoltà all'interno di una variabile per stabilire le celle del campo.
     const numberCell = parseInt(selectLevel.value);
-    //di questo valore ne faccio la radice quadrata in modo da avere il numero di celle per ogni riga e colonna
     const sideSquare = Math.sqrt(numberCell);
-    //associo SideSquare ad una variabile all'interno del css in modo da cambiare dinamicamente le celle alla selezione di ogni difficoltà
     gridElement.style.setProperty('--sideSquare', sideSquare);
 
+
+    // const myArray = [];
+    // for (let i = 1; i <= 16; i++) {
+    //     let randomNum;
+    //     do {
+    //         randomNum = randomNumber(1, 16);
+    //     } while (myArray.includes(randomNum)){
+    //         myArray.push(randomNum);
+    //     }
+    // }
+    
+
     for (let i = 1; i <= numberCell; i++) {
+
         const cellElement = document.createElement('div');
         cellElement.classList.add('cell');
     
@@ -27,5 +37,9 @@ playButton.addEventListener('click', function(){
             this.classList.toggle('active');
         })
     }
+    console.log(myArray);
 })
 
+function randomNumber (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
