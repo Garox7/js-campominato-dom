e41218e8fe222696ac6c271146cfe7f39ec396e3
Garox7 <giuseppe.garozzo2@icloud.com>
2022-11-05 18:12:30 +0100

    I start all over again

--- javascript/script.js
+++ javascript/script.js
@@ -1,50 +1,20 @@
+/*
+Il Gioco inizia da un popup di benvenuto che indica di selezionare la difficolà e il bottone play.
+Funzione al bottone play:
+    - Puliamo tutta la griglia (consigliato così da non avere griglie doppioni ad ogni click)
+    - Aggiungiamo la classe "hidden" al popup
+    - Rimuoviamo la classe "hidden" alla griglia
+
+    - Definiamo il livello di dificoltà selezionato prendendone il valore così da ottenere il numero di celle
+    - Definiamo la larghezza, nonchè l'altezza di ogni cella, andando a calcolare la radice quadrata del numero di celle
+    - Inseriamo il valore ricavato prima per attribuirlo ad una variabile css che determina la width e la height
+    
+    -Creiamo un'array contenente 16 numeri random che rappresenteranno le bombe (anche tramite funzione)
+        - utilizziamo una funzione di generazione di numeri random
+        - questi numeri saranno inseriti dentro l'array solo se non sono doppioni PER 16 VOLTE
+    
+*/
 const selectLevel = document.querySelector('#select-difficulty');
 const playButton = document.querySelector('.header__playgame');
 const gridElement = document.querySelector('.field__grid');
 const popupElement = document.querySelector('.popup');
-
-playButton.addEventListener('click', function(){
-    gridElement.replaceChildren();
-
-    popupElement.classList.add('hidden');
-    gridElement.classList.remove('hidden');
-
-    const numberCell = parseInt(selectLevel.value);
-    const sideSquare = Math.sqrt(numberCell);
-    gridElement.style.setProperty('--sideSquare', sideSquare);
-
-
-    const myArray = [];
-    for (let i = 1; i <= 16; i++) {
-        let randomNum;
-        do {
-            randomNum = randomNumber(1, 16);
-        } while (myArray.includes(randomNum)){
-            myArray.push(randomNum);
-        }
-    }
-    
-    // var prova = myArray.slice();
-    // console.log(prova);
-
-    for (let i = 1; i <= numberCell; i++) {
-
-
-        const cellElement = document.createElement('div');
-        cellElement.classList.add('cell');
-    
-        gridElement.append(cellElement);
-        cellElement.append([i]);
-
-        cellElement.addEventListener ('click', function() {
-            this.classList.toggle('active');
-        })
-    }
-    console.log(myArray);
-
-    function randomNumber (min, max) {
-        return Math.floor(Math.random() * (numberCell - 16 + 1)) + 16;
-    }
-
-
-})
